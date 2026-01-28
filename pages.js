e9ec0e1e4b95c4097e27fe4def32711ae975b779
Luiz Felipe <lfelipelopesti@gmail.com>
2026-01-27 21:10:53 -0300

    feat: update photo about me

--- pages.js
+++ pages.js
@@ -2,14 +2,28 @@ const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
 const repoURL = "https://github.com/luizfelipelopes/luizfelipelopes.github.io.git";
 
+// Increase Git HTTP buffer to handle larger files
+process.env.GIT_HTTP_BUFFER = "524288000"; // 500MB
+
 ghpages.publish(
   pathname,
   {
     branch: "master",
     repo: repoURL,
+    // Add message for the commit
+    message: "Deploy to GitHub Pages",
+    // Add .nojekyll file to bypass Jekyll processing
+    add: true,
   },
   (err) => {
-    if (err) console.log("ERROR: ", err);
-    else console.log("PUBLISHED");
+    if (err) {
+      console.log("ERROR: ", err);
+      console.log("\nTroubleshooting tips:");
+      console.log("1. Make sure you have a GitHub Personal Access Token set up");
+      console.log("2. Try running: git config --global http.postBuffer 524288000");
+      console.log("3. Check if you need to authenticate: git config --global credential.helper store");
+    } else {
+      console.log("PUBLISHED");
+    }
   }
 );
